ccbf253f6e5178052f02d1962df2aaa23d52e1f1
jonasf <jonas.falkevik@mobilearts.com>
2017-05-16 11:11:15 +0200

    Make rocksdb decied if we need to rebuild

--- build.sh
+++ build.sh
@@ -7,12 +7,5 @@ if [ ! -d ${ROCKSDB_DIR} ]; then
   	#git clone https://github.com/google/rocksdb.git
 	(cd c_src && git clone https://github.com/pundunlabs/rocksdb.git && cd rocksdb && git checkout $ROCKSDB_TAG)
 fi
-
-if [ -L "${ROCKSDB_DIR}/librocksdb.so" ]
-then
-	echo "librocksdb.so found."
-else
-	echo "librocksdb.so not found."
-	export INSTALL_PATH="."
-	(cd ${ROCKSDB_DIR} && make shared_lib)
-fi
+export INSTALL_PATH="."
+(cd ${ROCKSDB_DIR} && make shared_lib)
